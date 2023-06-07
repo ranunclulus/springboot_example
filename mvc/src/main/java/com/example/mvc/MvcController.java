68bc5ee057742706101c736721be428af5dcdea1
ranunclulus <morion002@gmail.com>
2023-06-07 14:26:19 +0900

    feat: mvc controller

---
+++ mvc/src/main/java/com/example/mvc/MvcController.java
@@ -0,0 +1,13 @@
+package com.example.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MvcController {
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
