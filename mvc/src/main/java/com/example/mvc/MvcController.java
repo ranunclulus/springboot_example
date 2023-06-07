0c956714018d70a92156449dbe80009e10acdd88
ranunclulus <morion002@gmail.com>
2023-06-07 14:32:02 +0900

    feat: Model and attribute example

--- mvc/src/main/java/com/example/mvc/MvcController.java
+++ mvc/src/main/java/com/example/mvc/MvcController.java
@@ -1,12 +1,15 @@
 package com.example.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MvcController {
     @RequestMapping("/")
-    public String home() {
+    // view가 활용을 하기 위한 모델
+    public String home(Model model) {
+        model.addAttribute("message", "hello, thymeleaf");
         return "home";
     }
 
