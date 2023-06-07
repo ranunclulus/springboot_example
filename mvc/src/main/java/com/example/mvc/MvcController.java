61937aba8838519d5a3ca31403b181b75579e154
ranunclulus <morion002@gmail.com>
2023-06-07 14:42:44 +0900

    feat: Object Model example

--- mvc/src/main/java/com/example/mvc/MvcController.java
+++ mvc/src/main/java/com/example/mvc/MvcController.java
@@ -1,5 +1,6 @@
 package com.example.mvc;
 
+import com.example.mvc.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +13,11 @@ public class MvcController {
         model.addAttribute("message", "hello, thymeleaf");
         return "home";
     }
-
+    @RequestMapping("/student")
+    public String student(Model model) {
+        model.addAttribute(
+                "object",
+                new Student("Huisu", "morion002@gmail.com"));
+        return "student";
+    }
 }
