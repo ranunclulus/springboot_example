43be021a1acf71f460ea092bb882487d423dc57a
ranunclulus <morion002@gmail.com>
2023-06-08 15:00:37 +0900

    fix: student service id 관리하는 nextID 변수 추가

--- crud/src/main/java/com/example/crud/StudentService.java
+++ crud/src/main/java/com/example/crud/StudentService.java
@@ -9,10 +9,12 @@ import java.util.List;
 @Service
 public class StudentService {
     // 복수의 StudentDto를 담는 변수
-    private List<StudentDto> studentDtoList = new ArrayList<>();
+    private final List<StudentDto> studentDtoList = new ArrayList<>();
+    private long nextId = 1L;
     // 새로운 StudentDto를 생성하는 메소
-    public void newStudentDto(long id, String name, String email) {
-        StudentDto studentDto = new StudentDto(id, name, email);
+    public StudentDto createStudent(String name, String email) {
+        StudentDto studentDto = new StudentDto(nextId++, name, email);
         studentDtoList.add(studentDto);
+        return studentDto;
     }
 }
