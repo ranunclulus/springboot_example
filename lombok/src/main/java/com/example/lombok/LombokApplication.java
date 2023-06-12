8ba7aff353c4879986c8b814421e922d5bbe940c
ranunclulus <morion002@gmail.com>
2023-06-12 11:51:50 +0900

    feat: lombok allargsConstructor

--- lombok/src/main/java/com/example/lombok/LombokApplication.java
+++ lombok/src/main/java/com/example/lombok/LombokApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LombokApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LombokApplication.class, args);
+		
+		//SpringApplication.run(LombokApplication.class, args);
+		StudentDto dto = new StudentDto(1L, "huisu", "huisu@gmail.com");
 	}
-
 }
---
+++ lombok/src/main/java/com/example/lombok/StudentDto.java
@@ -0,0 +1,11 @@
+package com.example.lombok;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class StudentDto {
+    private Long id;
+    private String name;
+    private String email;
+
+}
