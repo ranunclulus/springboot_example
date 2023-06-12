c60b4367137ed312e064e38b47d2e4817bfddc42
ranunclulus <morion002@gmail.com>
2023-06-12 14:28:38 +0900

    feat: create table

---
+++ lombok/1_ddl.sql
@@ -0,0 +1,13 @@
+create table students (
+    id integer,
+    name varchar(32),
+    email text
+);
+
+create table students_2 (
+    id integer,
+    username text,
+    first_name text,
+    last_name text,
+    email text
+)
