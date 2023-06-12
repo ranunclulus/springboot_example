1be41a85e38acb4796ff357f2bbf1d131f3441e2
ranunclulus <morion002@gmail.com>
2023-06-12 14:51:46 +0900

    fix: table with constraints

--- lombok/1_ddl.sql
+++ lombok/1_ddl.sql
@@ -10,4 +10,39 @@ create table students_2 (
     first_name text,
     last_name text,
     email text
+);
+
+-- not null constraints
+create table students_not_null (
+    id integer,
+    username text,
+    first_name text,
+    last_name text,
+    email text not null -- email은 null이 될 수 없다
+);
+
+-- unique constraints
+create table students_unique (
+    id integer,
+    username text unique,
+    first_name text,
+    last_name text,
+    email text not null
+);
+
+-- unique constraints
+create table students_pks (
+    id integer primary key autoincrement, -- id가 PK이고 자동으로 하나씩 올리면서 생성
+    username text unique,
+    first_name text,
+    last_name text,
+    email text not null
+);
+
+create table students_final (
+    id integer primary key autoincrement,
+    username text unique,
+    first_name text,
+    last_name text,
+    email text not null
 )
