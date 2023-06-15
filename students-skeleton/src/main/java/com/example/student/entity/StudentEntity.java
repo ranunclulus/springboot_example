3a98148631c6e19f00bc877c943cba684efb07a6
ranunclulus <morion002@gmail.com>
2023-06-15 13:40:48 +0900

    feat: entity create

---
+++ students-skeleton/src/main/java/com/example/student/entity/StudentEntity.java
@@ -0,0 +1,26 @@
+package com.example.student.entity;
+/* CREATE TABLE students (
+*   id INTEGER PRIMARY KEY AUTOINCREMENT,
+*   name TEXT,
+*   age INTEGER,
+*   phone TEXT,
+*   email TEXT
+* */
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "students")
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String phone;
+    private String email;
+}
+
