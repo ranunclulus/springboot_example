a9c5c5346317b3ffe3223dd166998b833b548412
ranunclulus <morion002@gmail.com>
2023-06-15 15:38:55 +0900

    feat: crud complete

--- students-skeleton/src/main/java/com/example/student/StudentController.java
+++ students-skeleton/src/main/java/com/example/student/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
@@ -35,37 +36,57 @@ public class StudentController {
     public String create(StudentDto dto) {
         StudentDto newDto = service.createStudent(dto);
         // Post Redirect Get Pattern
-        // View Resolver가 리다이렉트 String을 읽어서 경로를 저장해 주기 때문에 students/도 넣어 줘야 함
-        return("redirect: /students/" + newDto.getId());
+        // View Resolver가 리다이렉트 String을 읽어서 경로를 저장해 주기 때문에 students/도 넣어 줘야
+        return("redirect:/students/" + newDto.getId());
     }
 
     // id에 해당하는 StudentEntity의 read.html 응답
     @GetMapping("/{id}")
-    public String read() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String read(
+            @PathVariable("id") Long id,
+            Model model
+    ) {
+        model.addAttribute("student", service.readStudent(id));
+        return "read";
     }
 
     // id에 해당하는 StudentEntity의 update.html 응답
     @GetMapping("/{id}/update-view")
-    public String updateView(){
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String updateView(
+            @PathVariable("id") Long id,
+            Model model
+    ){
+        model.addAttribute("student", service.readStudent(id));
+        return "update";
     }
 
     // id에 해당하는 StudentEntity 수정 후 상세보기 페이지로
     @PostMapping("/{id}/update")
-    public String update() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String update(
+            @PathVariable("id") Long id,
+            StudentDto dto
+    ) {
+        StudentDto updateDto = service.updateStudent(id, dto);
+        return "redirect:/students/" + updateDto.getId();
     }
 
     // id에 해당하는 StudentEntity delete.html
     @GetMapping("/{id}/delete-view")
-    public String deleteView() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String deleteView(
+            @PathVariable("id") Long id,
+            Model model
+    ) {
+        model.addAttribute("student", service.readStudent(id));
+        return "delete";
     }
 
     // id에 해당하는 StudentEntity 삭제 후 홈페이지로
     @PostMapping("/{id}/delete")
-    public String delete() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String delete(
+            @PathVariable("id") Long id
+
+    ) {
+        service.deleteStudent(id);
+        return "redirect:/students";
     }
 }
--- students-skeleton/src/main/java/com/example/student/StudentService.java
+++ students-skeleton/src/main/java/com/example/student/StudentService.java
@@ -3,10 +3,13 @@ package com.example.student;
 import com.example.student.dto.StudentDto;
 import com.example.student.entity.StudentEntity;
 import com.example.student.repository.StudentRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -31,7 +34,16 @@ public class StudentService {
     }
 
     // READ
-    public void readStudent() {}
+    public StudentDto readStudent(Long id) {
+        Optional<StudentEntity> optionalEntity =
+                this.repository.findById(id);
+        if (optionalEntity.isPresent()) {
+            return StudentDto.fromEntity(optionalEntity.get());
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
 
     // READ ALL
     public List<StudentDto> readStudentAll() {
@@ -45,8 +57,24 @@ public class StudentService {
     }
 
     // UPDATE
-    public void updateStudent() {}
+    public StudentDto updateStudent(Long id, StudentDto studentDto) {
+        Optional<StudentEntity> optionalEntity
+                = repository.findById(id);
+
+        if(optionalEntity.isPresent()) {
+            StudentEntity targetEntity = optionalEntity.get();
+            targetEntity.setName(studentDto.getName());
+            targetEntity.setAge(studentDto.getAge());
+            targetEntity.setPhone(studentDto.getPhone());
+            targetEntity.setEmail(studentDto.getEmail());
+            return StudentDto.fromEntity(repository.save(targetEntity));
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
 
     // DELETE
-    public void deleteStudent() {}
+    public void deleteStudent(Long id) {
+        this.repository.deleteById(id);
+    }
 }
