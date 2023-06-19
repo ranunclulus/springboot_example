73049284450a16cc54899a06d3f577898ad8ce48
ranunclulus <morion002@gmail.com>
2023-06-19 14:15:04 +0900

    feat: mapping controller

---
+++ http/src/main/java/com/example/http/MappingController.java
@@ -0,0 +1,35 @@
+package com.example.http;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@Slf4j  // log 변수 만들어줌
+public class MappingController {
+//    private static final Logger logger = LoggerFactory.getLogger(MappingController.class);
+
+    // '/path'로 오는 GET 요청에 대해서 메소드를 실행하고 싶을 때
+    @RequestMapping(value = "/path", method = RequestMethod.GET)
+    public String getPath() {
+        log.info("GET /path");
+        return "index";
+    }
+
+    // '/path'로 오는 POST 요청에 대해서 메소드를 실행하고 싶을 때
+    @RequestMapping(value = "/path", method = RequestMethod.POST)
+    public String postPath() {
+        log.info("POST /path");
+        return "index";
+    }
+
+    // '/path'로 오는 PUT 또는 DELETE 요청에 대해서
+    // 메소드를 실행하고 싶을 때
+    @RequestMapping(value = "/path",
+            method = { RequestMethod.PUT, RequestMethod.DELETE })
+    public String putOrDeletePath() {
+        log.info("PUT or DELETE /path");
+        return "index";
+    }
+}
