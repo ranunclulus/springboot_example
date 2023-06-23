0cfea55e5948e3cc32f97ee4ed0fdc1abfe53c7e
ranunclulus <morion002@gmail.com>
2023-06-23 15:11:12 +0900

    feat: 404 error class

---
+++ contents-skeleton/src/main/java/com/example/contents/exceptions/Status404Exception.java
@@ -0,0 +1,7 @@
+package com.example.contents.exceptions;
+
+public class Status404Exception extends RuntimeException{
+    public Status404Exception(String message) {
+        super(message);
+    }
+}
