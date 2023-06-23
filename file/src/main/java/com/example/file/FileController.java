804a655f62ba1914ea6c9f732769d617a220362e
ranunclulus <morion002@gmail.com>
2023-06-23 10:19:54 +0900

    feat: path add

--- file/src/main/java/com/example/file/FileController.java
+++ file/src/main/java/com/example/file/FileController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 @Slf4j
 @RestController
 public class FileController {
@@ -16,7 +19,11 @@ public class FileController {
     public ResponseDto miltipart(
             @RequestParam("name") String name,
             @RequestParam("photo")MultipartFile multipartFile
-            ) {
+            ) throws IOException {
+        // 컴퓨터에서 파일 경로를 관리하기 위해 만들어진 인터페이스
+        Path uploadTo = Path.of("filename.png");
+        multipartFile.transferTo(uploadTo);
+
         ResponseDto response = new ResponseDto();
         response.setMessage("success");
         return response;
