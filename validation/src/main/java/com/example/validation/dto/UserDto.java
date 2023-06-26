22213a3e78732a167f5c172bcd74e8085508271d
ranunclulus <morion002@gmail.com>
2023-06-26 13:45:35 +0900

    fix: email column add email annotation

--- validation/src/main/java/com/example/validation/dto/UserDto.java
+++ validation/src/main/java/com/example/validation/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.example.validation.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -9,6 +10,7 @@ public class UserDto {
 
     @NotBlank // 비어 있지 않음
     private String username;
+    @Email // 형식이 이메일이어야 한다
     private String email;
     private String phone;
 }
