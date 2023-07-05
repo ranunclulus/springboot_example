5c73f5aae4dccfd4e549ae5dc376b1cb259f22bd
ranunclulus <morion002@gmail.com>
2023-07-05 14:33:16 +0900

    fix: beear get dto

--- client/src/main/java/com/example/client/BeerClientService.java
+++ client/src/main/java/com/example/client/BeerClientService.java
@@ -13,14 +13,14 @@ public class BeerClientService {
         String url = "https://random-data-api.com/api/v2/beers";
         // 어떤 HTTP Method로 요청을 보낼지를 get() post() 메소드 순으로 결정
         // 만일 다른 메소드를 쓰고 싶다면 method() 사
-        String response = webClient.get() // webClient.method(HttpMethod.GET)\
+        BeerGetDto response = webClient.get() // webClient.method(HttpMethod.GET)\
                 .uri(url) // 요청 경로 설정
                 .header("x-test", "header") // 요청 헤더 추가
                 // GET 요청이라 Body 없이
                 .retrieve() // 여기 전까지는 요청을 정의한 부분
                 // 이제부터 정의하는 건 응답을 어떻게 처리할 것인지
-                .bodyToMono(String.class) // 응답이 한 번 돌아올 것이며 String으로 받을 거임
+                .bodyToMono(BeerGetDto.class) // 응답이 한 번 돌아올 것이며 String으로 받을 거임
                 .block(); // 동기식으로 처리하겠다
-        log.info(response);
+        log.info(response.toString());
     }
 }
