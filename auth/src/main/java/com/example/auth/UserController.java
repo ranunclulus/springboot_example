a74c8cc23b2f1e9e9db66687d34d75ef9d4c262e
ranunclulus <morion002@gmail.com>
2023-07-06 13:45:55 +0900

    feat: html format

--- auth/src/main/java/com/example/auth/UserController.java
+++ auth/src/main/java/com/example/auth/UserController.java
@@ -1,4 +1,20 @@
 package com.example.auth;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+    // 1. login 페이지로 온다
+    // 2. login 페이지에 아이디 비밀번호를 입력한다
+    // 3. 성공하면 my-profile로 이동한다
+    @GetMapping("/login")
+    public String loginForn() {
+        return "login-form";
+    }
+
+    @GetMapping("/my-profile")
+    public String myProfile() {
+        return "my-profile";
+    }
 }
