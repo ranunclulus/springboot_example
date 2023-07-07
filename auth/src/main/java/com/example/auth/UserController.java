fc6320dd3b6c79fe152e22160cd2a38ba1324c56
ranunclulus <morion002@gmail.com>
2023-07-07 14:46:43 +0900

    fix: code convention

--- auth/src/main/java/com/example/auth/UserController.java
+++ auth/src/main/java/com/example/auth/UserController.java
@@ -3,6 +3,7 @@ package com.example.auth;
 import com.example.auth.entity.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -13,42 +14,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
-@Controller
+@Controller  // 로그인 페이지를 보여줄려고
 @RequestMapping("/users")
 public class UserController {
-    // 1. login 페이지로 온다
-    // 2. login 페이지에 아이디 비밀번호를 입력한다
-    // 3. 성공하면 my-profile로 이동한다
+
+    // 1. login 페이지로 온다.
+    // 2. login 페이지에 아이디 비밀번호를 입력한다.
+    // 3. 성공하면 my-profile 로 이동한다.
+
+    // 로그인 페이지를 위한 GetMapping
     @GetMapping("/login")
-    public String loginForn() {
+    public String loginForm() {
         return "login-form";
     }
 
+    // 로그인 성공 후 로그인 여부를 판단하기 위한 GetMapping
     @GetMapping("/my-profile")
-    public String myProfile(Authentication authentication) {
-        log.info(authentication.getName());
+    public String myProfile(
+            Authentication authentication
+    ) {
+//        log.info(authentication.getName());
+//        log.info(((User) authentication.getPrincipal()).getUsername());
         CustomUserDetails userDetails
                 = (CustomUserDetails) authentication.getPrincipal();
         log.info(userDetails.getUsername());
         log.info(userDetails.getEmail());
+//        log.info(SecurityContextHolder.getContext().getAuthentication().getName());
         return "my-profile";
     }
 
-    // 1. 사용자가 register 페이지로 온다
-    // 2. 사용자가 register 페이지에 id, password, password확인을 입력
-    // 3. register 페이지에서 /users/register로 post 요청
-    // 4. UserDetailsManager에 새로운 사용자 정보 추가
+    // 1. 사용자가 register 페이지로 온다.
+    // 2. 사용자가 register 페이지에 ID, 비밀번호, 비밀번호 확인을 입력
+    // 3. register 페이지에서 /users/register 로 POST 요청
+    // 4. UserDetailsManager 에 새로운 사용자 정보 추가
+
     @GetMapping("/register")
     public String registerForm() {
         return "register-form";
     }
 
-
-
     // 어떻게 사용자를 관리하는지는
     // interface 기반으로 의존성 주입
     private final UserDetailsManager manager;
     private final PasswordEncoder passwordEncoder;
+
 
     public UserController(
             UserDetailsManager manager,
@@ -68,9 +77,14 @@ public class UserController {
             log.info("password match!");
             // username 중복도 확인해야 하지만,
             // 이 부분은 Service 에서 진행하는 것도 나쁘지 않아보임
-            manager.createUser(User.withUsername(username)
+//            manager.createUser(User.withUsername(username)
+//                    .password(passwordEncoder.encode(password))
+//                    .build());
+            manager.createUser(CustomUserDetails.builder()
+                    .username(username)
                     .password(passwordEncoder.encode(password))
                     .build());
+
             return "redirect:/users/login";
         }
         log.warn("password does not match...");
