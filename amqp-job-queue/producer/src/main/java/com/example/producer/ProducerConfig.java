2dd26a17cddcf4b7a40cff6af672924464f2b3c9
ranunclulus <morion002@gmail.com>
2023-07-12 14:41:24 +0900

    feat: dtos and producer config queue method

---
+++ amqp-job-queue/producer/src/main/java/com/example/producer/ProducerConfig.java
@@ -0,0 +1,17 @@
+package com.example.producer;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProducerConfig {
+    @Bean
+    public Queue queue() {
+        return new Queue(
+                "boot.amqp.worker-queue",
+                true,
+                false,
+                true);
+    }
+}
