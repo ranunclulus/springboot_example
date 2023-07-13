e21b225deb976b791b90a3abcf8928f46b759c04
ranunclulus <morion002@gmail.com>
2023-07-13 14:05:40 +0900

    fix: json convert

--- websocket/chat-skeleton/src/main/java/com/example/chat/SimpleChatHandler.java
+++ websocket/chat-skeleton/src/main/java/com/example/chat/SimpleChatHandler.java
@@ -1,5 +1,7 @@
 package com.example.chat;
 
+import com.example.chat.dto.ChatMessage;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -7,6 +9,7 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,12 +27,22 @@ public class SimpleChatHandler extends TextWebSocketHandler {
         log.info("connected with session id: {}, total sessions: {}", session.getId(), sessions.size());
     }
 
+    public void broadcast(String message) throws IOException {
+        for (WebSocketSession connected: sessions) {
+            connected.sendMessage(new TextMessage(message));
+        }
+    }
+
     @Override
     protected void handleTextMessage(
             WebSocketSession session,
             TextMessage message) throws Exception {
         String payload = message.getPayload();
+        ChatMessage chatMessage = new Gson().fromJson(payload, ChatMessage.class);
         log.info("recieved: {}", payload);
+        for(WebSocketSession conneted: sessions) {
+            conneted.sendMessage(message);
+        }
     }
 
     @Override
