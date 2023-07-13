52a6ca5a65e1ec5e6dd14472245e6176ae56213f
ranunclulus <morion002@gmail.com>
2023-07-13 15:33:16 +0900

    feat: web socket config

--- websocket/stomp-skeleton/src/main/java/com/example/stomp/socket/WebSocketMapping.java
+++ websocket/stomp-skeleton/src/main/java/com/example/stomp/socket/WebSocketMapping.java
@@ -3,12 +3,16 @@ package com.example.stomp.socket;
 import com.example.stomp.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
+
 
 @Slf4j
 @Controller
@@ -20,8 +24,14 @@ public class WebSocketMapping {
 
     // RequestMapping 에 대응
     @MessageMapping("/chat")
-    public void sendChat(ChatMessage chatMessage) {
+    public void sendChat(
+            ChatMessage chatMessage,
+            @Headers Map<String, Object> headers,
+            @Header("nativeHeaders") Map<String, Object> nativeHeaders
+            ) {
         log.info(chatMessage.toString());
+        log.info(headers.toString());
+        log.info(nativeHeaders.toString());
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         chatMessage.setTime(time);
         simpMessagingTemplate.convertAndSend(
