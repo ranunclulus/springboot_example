8a42cd91a117ad283c259940f2c0ce1e9ead3da4
ranunclulus <morion002@gmail.com>
2023-07-14 13:20:57 +0900

    feat: article 8082 port route setting

--- gateway/gateway/src/main/java/com/example/gateway/RoutingConfig.java
+++ gateway/gateway/src/main/java/com/example/gateway/RoutingConfig.java
@@ -1,4 +1,19 @@
 package com.example.gateway;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RoutingConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("articles", predicate -> predicate
+                        .path("/articles/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
 }
