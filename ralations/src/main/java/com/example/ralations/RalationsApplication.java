0fefbe93e55ef6cfb6c9b1418c28eeba4daa5231
ranunclulus <morion002@gmail.com>
2023-07-18 13:07:20 +0900

    feat: prepared relations

---
+++ ralations/src/main/java/com/example/ralations/RalationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ralations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalationsApplication.class, args);
+	}
+
+}
---
+++ ralations/src/test/java/com/example/ralations/RalationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ralations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RalationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
