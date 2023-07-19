0afec2eae680af1fb1f9bd14ccdea1b456fd2c1b
ranunclulus <morion002@gmail.com>
2023-07-19 11:41:08 +0900

    feat: join prepared

---
+++ db-join-practice/src/main/java/com/example/dbjoinpractice/DbJoinPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbjoinpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbJoinPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbJoinPracticeApplication.class, args);
+    }
+
+}
